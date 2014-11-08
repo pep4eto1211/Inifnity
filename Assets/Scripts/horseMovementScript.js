45b7ed19acffbb4ac552e8d3d74906d63ee1cbb5
tonynho <tonynho010@gmail.com>
2014-11-08 17:20:28 +0200

    add horse functionallity

---
+++ Assets/Scripts/horseMovementScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var speed = 5.0f;
+var range = 10.0f;					// move [range] units to the left and the right
+
+private var xStart = 0.0f;
+private var direction = 1;
+
+function Start () {
+	xStart = this.transform.position.x;
+}
+
+function Update () {
+	var xCurrent = this.transform.position.x;
+	
+	if((xCurrent >= xStart + range) || (xCurrent <= xStart - range)) {
+		direction *= -1;
+	}
+	
+	this.transform.position.x += direction * speed * Time.deltaTime;
+}
