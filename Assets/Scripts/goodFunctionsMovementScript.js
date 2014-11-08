8d5f7bd8c20510568ddb21151899372279e9668a
tonynho <tonynho010@gmail.com>
2014-11-08 17:49:15 +0200

    fix movement script

--- Assets/Scripts/goodFunctionsMovementScript.js
+++ Assets/Scripts/goodFunctionsMovementScript.js
@@ -5,15 +5,19 @@ var range = 10.0f;					// move [range] units to the left and the right
 
 private var xStart = 0.0f;
 private var direction = 1;
+private var leftBorder = 0.0f;
+private var rightBorder = 0.0f;
 
 function Start () {
 	xStart = this.transform.position.x;
+	leftBorder = xStart - range;
+	rightBorder = xStart + range;
 }
 
 function Update () {
 	var xCurrent = this.transform.position.x;
 	
-	if((xCurrent >= xStart + range) || (xCurrent <= xStart - range)) {
+	if(xCurrent < leftBorder || xCurrent > rightBorder) {
 		direction *= -1;
 	}
 	
