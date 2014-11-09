25de24a5b35aa5e3aa27d66be009889e6a61b023
tonynho <tonynho010@gmail.com>
2014-11-09 15:20:43 +0200

    major commit

---
+++ Assets/movingGroundScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(other: Collider2D) {
+	other.transform.parent = this.transform.parent;
+}
+
+function OnTriggerExit2D(other: Collider2D) {
+	other.transform.parent = null;
+}
