bac605edc532d809d3be6ebab95a08852b8a2e81
pep4eto1211 <pepsiko@mail.bg>
2014-11-09 15:18:41 +0200

    First Cutscene

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void changeLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
