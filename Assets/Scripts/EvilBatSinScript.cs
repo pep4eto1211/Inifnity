344cc91fa61535297c345a3a2782e90e228ae1ca
pep4eto1211 <pepsiko@mail.bg>
2014-11-09 13:41:38 +0200

    Vs raboti kakto trqbva za iznenada

---
+++ Assets/Scripts/EvilBatSinScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvilBatSinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ActionFunction(int value)
+	{
+		GameObject.FindWithTag ("Player").SendMessage ("Functionaized", Mathf.Sin(value));
+		GameObject.Find ("Functions").SendMessage ("destroyEnemy", this.gameObject);
+	}
+	
+	public void ActionFunctionPassive(int value)
+	{
+		GameObject.FindWithTag ("Stones").SendMessage ("Functionaized", Mathf.Sin(value));
+		GameObject.Find ("Functions").SendMessage ("destroyEnemy", this.gameObject);
+	}
+}
