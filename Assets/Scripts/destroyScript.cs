28268d739f536a267823b3baf7146fb5e8d62609
GeorgiAnchev <georgianchev51@gmail.com>
2014-11-09 12:06:06 +0200

    more

--- Assets/Scripts/destroyScript.cs
+++ Assets/Scripts/destroyScript.cs
@@ -8,9 +8,17 @@ public class destroyScript : MonoBehaviour {
 	
 	}
 
+	public GameObject projectile;
+
 	public void destroyEnemy(GameObject objectToDestroy)
 	{
 		GameObject.Find ("DamageZone").SendMessage ("removeFromList", objectToDestroy);
+		/*
+		GameObject.Find ("Explosion").transform.position = objectToDestroy.transform.position;
+		GameObject.Find ("Explosion").particleEmitter.emit = true;
+		*/
+		GameObject newGameObject = Instantiate (projectile, objectToDestroy.transform.position, Quaternion.identity) as GameObject;
+		newGameObject.particleEmitter.emit = true;
 		Destroy (objectToDestroy);
 	}
 
