bdb13961fad3fd179c0f27758d1b4e1ec6108e9c
GeorgiAnchev <georgianchev51@gmail.com>
2014-11-09 02:02:07 +0200

    finished intro lvl

---
+++ Assets/Scripts/portalScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class portalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D e)
+	{
+		if (e.gameObject.tag == "Player") 
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
